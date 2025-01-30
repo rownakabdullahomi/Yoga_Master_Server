a479d6f470d285dcab5880a9e695ca97cbe37cce
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-01-31 01:55:51 +0600

    post payment info done

--- index.js
+++ index.js
@@ -159,6 +159,39 @@ async function run() {
             })
         })
 
+        // post payment info to db
+        app.post('/payment-info', verifyJWT, async (req, res) => {
+            const paymentInfo = req.body;
+            const classesId = paymentInfo.classesId;
+            const userEmail = paymentInfo.userEmail;
+            const singleClassId = req.query.classId;
+            let query;
+            // const query = { classId: { $in: classesId } };
+            if (singleClassId) {
+                query = { classId: singleClassId, email: userEmail };
+            } else {
+                query = { classId: { $in: classesId } };
+            }
+            const classesQuery = { _id: { $in: classesId.map(id => new ObjectId(id)) } }
+            const classes = await classesCollection.find(classesQuery).toArray();
+            const newEnrolledData = {
+                userEmail: userEmail,
+                classesId: classesId.map(id => new ObjectId(id)),
+                transactionId: paymentInfo.transactionId,
+            }
+            const updatedDoc = {
+                $set: {
+                    totalEnrolled: classes.reduce((total, current) => total + current.totalEnrolled, 0) + 1 || 0,
+                    availableSeats: classes.reduce((total, current) => total + current.availableSeats, 0) - 1 || 0,
+                }
+            }
+            // const updatedInstructor = await userCollection.find()
+            const updatedResult = await classesCollection.updateMany(classesQuery, updatedDoc, { upsert: true });
+            const enrolledResult = await enrolledCollection.insertOne(newEnrolledData);
+            const deletedResult = await cartCollection.deleteMany(query);
+            const paymentResult = await paymentCollection.insertOne(paymentInfo);
+            res.send({ paymentResult, deletedResult, enrolledResult, updatedResult });
+        })
 
 
 
